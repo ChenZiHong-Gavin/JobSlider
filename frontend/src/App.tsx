7a08731e82b51edc50f7a96ad34e6687c72838c9
chenzihong <522023320011@smail.nju.edu.cn>
2025-06-11 20:53:20 +0800

    init frontend

---
+++ frontend/src/App.tsx
@@ -0,0 +1,20 @@
+import './App.css'
+import { Card, CardContent, CardDescription, CardHeader, CardTitle } from '@/components/ui/card'
+
+function App() {
+
+  return (
+    <div>
+      <Card>
+        <CardHeader>
+          <CardTitle>JobSlider</CardTitle>
+        </CardHeader>
+        <CardContent>
+          <CardDescription>面试百词斩</CardDescription>
+        </CardContent>
+      </Card>
+    </div>
+  )
+}
+
+export default App
